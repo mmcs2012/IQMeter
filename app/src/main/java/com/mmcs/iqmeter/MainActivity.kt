e1354c25a1af8d205da447a3d7151cc5c213de60
Kirill Petukhov <mmcs@yandex.ru>
2019-04-13 20:39:30 +0300

    Создана разметка Activity, добавлен обработчик вводимых данных.

--- app/src/main/java/com/mmcs/iqmeter/MainActivity.kt
+++ app/src/main/java/com/mmcs/iqmeter/MainActivity.kt
@@ -2,11 +2,18 @@ package com.mmcs.iqmeter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var text = inputField.text
+        startButton.setOnClickListener{
+            if (text.isEmpty()) resultTextView.text = "Введите текст"
+            else resultTextView.text = "Привет, $text"
+        }
     }
 }
